0fbdf7b6bb72591592f6da1ec2b27d98e62acd53
Khasanov Magomed <94225626+nohvu@users.noreply.github.com>
2022-06-15 20:48:51 +0300

    Update index.tsx

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -10,37 +10,53 @@ type IProps = {
 }
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
+const FirstComponent = memo(({ name, age }: IUser) => {
+  return (
     <div>
-        my name is {name}, my age is {age}
+      my name is {name}, my age is {age}
     </div>
-);
+  );
+});
 
 // functional component
-const SecondComponent = ({ user: { name, age } }: IProps) => (
+const SecondComponent = memo(({ user: { name, age } }: IProps) => {
+  return (
     <div>
-        my name is {name}, my age is {age}
+      my name is {name}, my age is {age}
     </div>
-);
+  );
+}, (prevProps, nextProps) => prevProps.user.name === nextProps.user.name && prevProps.user.age === nextProps.user.age);
 
 // class component
 class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
+  shouldComponentUpdate(nextProps) {
+    if (this.props.name === nextProps.name && this.props.age === nextProps.age) {
+      return false;
     }
+    return true;
+  }
+  render() {
+    return (
+      <div>
+        my name is {this.props.name}, my age is {this.props.age}
+      </div>
+    );
+  }
 }
 
 // class component
 class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
+  shouldComponentUpdate(nextProps) {
+    if (this.props.user.name === nextProps.user.name && this.props.user.age === nextProps.user.age) {
+      return false;
     }
+    return true;
+  }
+  render() {
+    return (
+      <div>
+        my name is {this.props.user.name}, my age is {this.props.user.age}
+      </div>
+    );
+  }
 }
