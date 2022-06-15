68cbf2e6d986966fe4b07cc86c06d695b30a8844
Khasanov Magomed <94225626+nohvu@users.noreply.github.com>
2022-06-15 21:06:41 +0300

    Update index.jsx

--- task_4/index.jsx
+++ task_4/index.jsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import { Component, createRef, useState, forwardRef, useImperativeHandle} from 'react';
 
 class MainComponent extends Component {
     myRef = createRef(); // create simple ref
@@ -15,14 +15,12 @@ class MainComponent extends Component {
     }
 };
 
-class ChildComponent extends Component {
-    state = { isActive: true };
+const ChildComponent = forwardRef((_, ref) => {
+  const [isActive, setIsActive] = useState(true)
+  
+  useImperativeHandle(ref, () => ({
+    toggleButton: () => setIsActive((prev) => !prev)
+  }));
 
-    toggleButton = () => this.setState({ isActive: !this.state.isActive });
-
-    render() {
-        return (
-            this.state.isActive ? <div>child component</div> : null
-        );
-    }
-};
+  return isActive ? <div>child component</div> : null
+})
