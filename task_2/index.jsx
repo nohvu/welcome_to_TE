7d989fb6bcc26ddadf69a34142647fcd71966734
Khasanov Magomed <94225626+nohvu@users.noreply.github.com>
2022-06-15 21:30:35 +0300

    Update index.jsx

--- task_2/index.jsx
+++ task_2/index.jsx
@@ -1,4 +1,4 @@
-import { Fragment, memo } from 'react';
+import { Fragment, memo, useCallback } from 'react';
 
 const MainComponent = () => {
     const makeLog = useCallback(() => console.log("hi from MainComponent"), []); // function to make logs from MainComponent
